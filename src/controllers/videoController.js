de87558f2b2a07a8d9f5771e298d8782ed13b893
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-02-22 01:38:19 +0900

    Edit Video Title

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,19 +29,21 @@ export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 
-export const edit = (req, res) =>
-  res.render("edit", { pageTitle: "Edit Video" });
-
-export const search = (req, res) => res.send("Search");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Video");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 
-export const upload = (req, res) => res.send("Upload");
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
