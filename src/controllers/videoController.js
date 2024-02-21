e8b026ea9a40a9def362119c22590a697c43f643
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-02-21 21:28:51 +0900

    Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,9 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 
-export const edit = (req, res) => res.render("edit");
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
 
 export const search = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => {
