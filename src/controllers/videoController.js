98ee6360926dca642028c33f74a8b03b54378ff1
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-02-24 20:53:07 +0900

    Statics
    You can create a function

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,7 +33,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags,
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -49,9 +49,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       createdAt: Date.now(),
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags: Video.formatHashtags(hashtags),
       meta: {
         views: 0,
         rating: 0,
