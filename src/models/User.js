70f4ca48785bcc7e8ed0067937e9f0f5ad33a40c
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-03-02 22:46:09 +0900

    GIthub Login Part Six

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
+  username: { type: String, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
