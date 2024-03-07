f31bc71383053e14fe325596d43c458c856aabb8
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-03-07 23:41:54 +0900

    Styles Conclusion

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -226,7 +226,13 @@ export const remove = (req, res) => res.send("Remove User");
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   if (!user) {
     return res.status(404).render("404", { pageTitle: "No User Found" });
   }
