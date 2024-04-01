9ffbdd4e6d5ad47007cfe6b59f33a6a295aee4e5
Sun3577 <151902933+Sun3577@users.noreply.github.com>
2024-04-01 23:07:32 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -28,7 +28,12 @@ app.use(
 app.use(flash());
 app.use(localsMiddleware);
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
